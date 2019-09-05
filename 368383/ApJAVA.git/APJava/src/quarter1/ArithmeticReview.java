4125af218ee789ee7e419fa34fd3a1e9a2c6b2b8
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-05 08:16:20 -0400

    update 1

--- 368383/ApJAVA.git/APJava/src/quarter1/ArithmeticReview.java
+++ 368383/ApJAVA.git/APJava/src/quarter1/ArithmeticReview.java
@@ -7,23 +7,17 @@ public class ArithmeticReview {
 		int secondNumber = 8;
 		
 		System.out.println("9+8=17 "+(firstNumber+secondNumber));
-		System.out.println("");
 		System.out.println("9-8=1 "+(firstNumber-secondNumber));
-		System.out.println("");
 		System.out.println("9x8=72 "+(firstNumber*secondNumber));
-		System.out.println("");
 		System.out.println("9/8=1 "+(firstNumber/secondNumber));
-		System.out.println("");
 		System.out.println("9%8=1 "+(firstNumber%secondNumber));
+		System.out.println("");
+
 		//OUTPUT
-//		9+8=17 17
-//
+//				9+8=17 17
 //				9-8=1 1
-//
 //				9x8=72 72
-//
 //				9/8=1 1
-//
 //				9%8=1 1
 
 		//double task 2
@@ -37,23 +31,17 @@ public class ArithmeticReview {
 		firNumber = 9.4;
 		secNumber = 8.2;	
 		System.out.println("9.4+8.2=17.6 "+(firNumber+secNumber));
-		System.out.println("");
 		System.out.println("9.4-8.2=1.2"+(firNumber-secNumber));
-		System.out.println("");
 		System.out.println("9.4*8.2=77.08 "+(firNumber*secNumber));
-		System.out.println("");
 		System.out.println("9.4/8.2=1.1463414634146343 "+(firNumber/secNumber));
-		System.out.println("");
 		System.out.println("9.4/8.2=1.2"+(firNumber%secNumber));
+		System.out.println("");
+
 		//OUTPUT
-//		9.4+8.2=17.6 17.6
-//
+//				9.4+8.2=17.6 17.6
 //				9.4-8.2=11.200000000000001
-//
 //				9.4*8.2=77.08 77.08
-//
 //				9.4/8.2=1.15 1.1463414634146343
-//
 //				9.4/8.2=1 1.200000000000001
 
 		boolean first;
@@ -65,16 +53,14 @@ public class ArithmeticReview {
 	first = true; 
 	second = false; 
 	System.out.println("AND value " + (first&second));
-	System.out.println("");
 	System.out.println("OR value " + (first||second));
-	System.out.println("");
 	System.out.println("AND NOT value " + !(first&second));
+	System.out.println("");
 
-	//OUTPUT
+
+//OUTPUT
 //	AND value false
-//
 //	OR value true
-//
 //	AND NOT value true
 
 	}
