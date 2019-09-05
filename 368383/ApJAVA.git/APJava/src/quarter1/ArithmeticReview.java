f0c83972ac3e925ec08ea1694b445119afd3ebe7
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-05 08:13:17 -0400

    Finished Second Assignment

--- 368383/ApJAVA.git/APJava/src/quarter1/ArithmeticReview.java
+++ 368383/ApJAVA.git/APJava/src/quarter1/ArithmeticReview.java
@@ -27,17 +27,24 @@ public class ArithmeticReview {
 //				9%8=1 1
 
 		//double task 2
-		double firNumber = 9.4;
-		double secNumber = 8.2;	
+		double firNumber;
+		double secNumber;
+//		System.out.println("Value of firNumber "+ firNumber);
+//		System.out.println("");
+//		System.out.println("Value of secNumber "+ secNumber);
+//		System.out.println("");
+		// default value for double is null
+		firNumber = 9.4;
+		secNumber = 8.2;	
 		System.out.println("9.4+8.2=17.6 "+(firNumber+secNumber));
 		System.out.println("");
-		System.out.println("9.4-8.2=1"+(firNumber-secNumber));
+		System.out.println("9.4-8.2=1.2"+(firNumber-secNumber));
 		System.out.println("");
 		System.out.println("9.4*8.2=77.08 "+(firNumber*secNumber));
 		System.out.println("");
-		System.out.println("9.4/8.2=1.15 "+(firNumber/secNumber));
+		System.out.println("9.4/8.2=1.1463414634146343 "+(firNumber/secNumber));
 		System.out.println("");
-		System.out.println("9.4/8.2=1 "+(firNumber%secNumber));
+		System.out.println("9.4/8.2=1.2"+(firNumber%secNumber));
 		//OUTPUT
 //		9.4+8.2=17.6 17.6
 //
@@ -49,6 +56,26 @@ public class ArithmeticReview {
 //
 //				9.4/8.2=1 1.200000000000001
 
+		boolean first;
+		boolean second; 
+//		System.out.println("boolean first value "+ first);
+//		System.out.println("boolean second value "+ second);
+		//boolean default values are null 
 		
+	first = true; 
+	second = false; 
+	System.out.println("AND value " + (first&second));
+	System.out.println("");
+	System.out.println("OR value " + (first||second));
+	System.out.println("");
+	System.out.println("AND NOT value " + !(first&second));
+
+	//OUTPUT
+//	AND value false
+//
+//	OR value true
+//
+//	AND NOT value true
+
 	}
 }
