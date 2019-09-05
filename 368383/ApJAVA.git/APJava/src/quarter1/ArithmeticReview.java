7f9ddeda2221830444ceac2396d48426e63ff3d7
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-05 08:05:18 -0400

    adfafda

---
+++ 368383/ApJAVA.git/APJava/src/quarter1/ArithmeticReview.java
@@ -0,0 +1,54 @@
+package quarter1;
+//Jonathan Young, 9/5/19, APJava, Period 1
+public class ArithmeticReview {
+	public static void main(String[] args) {
+		//integer task 1
+		int firstNumber = 9;
+		int secondNumber = 8;
+		
+		System.out.println("9+8=17 "+(firstNumber+secondNumber));
+		System.out.println("");
+		System.out.println("9-8=1 "+(firstNumber-secondNumber));
+		System.out.println("");
+		System.out.println("9x8=72 "+(firstNumber*secondNumber));
+		System.out.println("");
+		System.out.println("9/8=1 "+(firstNumber/secondNumber));
+		System.out.println("");
+		System.out.println("9%8=1 "+(firstNumber%secondNumber));
+		//OUTPUT
+//		9+8=17 17
+//
+//				9-8=1 1
+//
+//				9x8=72 72
+//
+//				9/8=1 1
+//
+//				9%8=1 1
+
+		//double task 2
+		double firNumber = 9.4;
+		double secNumber = 8.2;	
+		System.out.println("9.4+8.2=17.6 "+(firNumber+secNumber));
+		System.out.println("");
+		System.out.println("9.4-8.2=1"+(firNumber-secNumber));
+		System.out.println("");
+		System.out.println("9.4*8.2=77.08 "+(firNumber*secNumber));
+		System.out.println("");
+		System.out.println("9.4/8.2=1.15 "+(firNumber/secNumber));
+		System.out.println("");
+		System.out.println("9.4/8.2=1 "+(firNumber%secNumber));
+		//OUTPUT
+//		9.4+8.2=17.6 17.6
+//
+//				9.4-8.2=11.200000000000001
+//
+//				9.4*8.2=77.08 77.08
+//
+//				9.4/8.2=1.15 1.1463414634146343
+//
+//				9.4/8.2=1 1.200000000000001
+
+		
+	}
+}
