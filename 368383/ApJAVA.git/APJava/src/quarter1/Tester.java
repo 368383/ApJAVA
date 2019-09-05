df3d54138781491a3f83bd5c9411dc3e780a4dc4
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-05 07:59:28 -0400

    seco

--- 368383/ApJAVA.git/APJava/src/quarter1/Tester.java
+++ 368383/ApJAVA.git/APJava/src/quarter1/Tester.java
@@ -11,7 +11,7 @@ public class Tester {
 	System.out.println("Problem 2: "+ (57.2*((double)i1/i2)+1));
 	System.out.println("Problem 3: "+ (15-i1*(d1*3)+4));
 	System.out.println("Problem 4: "+ (15-i1*((int)(d1*3))+4));
-	System.out.println("Problem 5: "+ (15-(int)(i1*(d1*3)+4)));
+	System.out.println("Problem 5: "+ (15- i1*((int)d1*3) + 4));
 
 	}
 }
