97034813703cac9d93d9ab32067ae24bc2f3b625
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-09 08:31:05 -0400

    Final Copy

--- 368383/ApJAVA.git/APJava/src/quarter1/leapYear.java
+++ 368383/ApJAVA.git/APJava/src/quarter1/leapYear.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class leapYear {
 	public static void main(String[] args) {
-		boolean breaker = true;
-		Scanner sc = new Scanner(System.in);
-		double year = sc.nextInt();
-		while (year > 1582 && breaker == true) {
+		while (1 == 1) {
+			boolean breaker = true;
+			Scanner sc = new Scanner(System.in);
+			double year = sc.nextInt();
+			while (year > 1582 && breaker == true) {
 
-			if (((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0))) {
-				System.out.println(year + " is a leap year");
-				breaker = false;
-				break;
-			} else {
-				System.out.println(year + " is not a leap year");
-				breaker = false;
+				if (((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0))) {
+					System.out.println(year + " is a leap year");
+					breaker = false;
+					break;
+				} else {
+					System.out.println(year + " is not a leap year");
+					breaker = false;
+					break;
+				}
+			}
+			while (breaker == true) {
+				System.out.println("Error");
 				break;
 			}
 		}
-		while (breaker == true) {
-			System.out.println("Error");
-			break;
-		}
-
 	}
-
 }
