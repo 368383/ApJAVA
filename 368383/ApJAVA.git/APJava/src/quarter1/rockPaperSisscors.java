2d5a00b58fd2dda8ea1a635cf2aa708839be4f12
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-13 08:24:35 -0400

    FULL POINTS - Final Rock Paper Sisscors complete with invalid inputs countermeasures.

---
+++ 368383/ApJAVA.git/APJava/src/quarter1/rockPaperSisscors.java
@@ -0,0 +1,58 @@
+package quarter1;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class rockPaperSisscors {
+
+	public static void main(String[] args) {
+		boolean con = true;
+		int win = 0;
+		int tie = 0;
+		int loss = 0;
+		while (con == true) {
+			// TODO Auto-generated method stub
+			System.out.println("1 for rock, 2 for paper, 3 for scissor | 0 for to stop");
+			Scanner user = new Scanner(System.in);
+			int userInput = user.nextInt();
+			int cpuInput = ThreadLocalRandom.current().nextInt(1, 3);
+			if ((!(userInput == 0) && userInput < 1) || userInput > 3) {
+				System.out.println("INVALD INPUT");
+				continue;
+			}
+			if (!(userInput == 0) && userInput == cpuInput) {
+				tie++;
+				System.out.println("TIE" + " | Computer Input: " + cpuInput + " | Your Input " + userInput);
+				continue;
+			}
+			if (!(userInput == 0) && userInput <= 2) {
+				if (cpuInput == 1) {
+					win++;
+					System.out.println("WIN" + " | Computer Input: " + cpuInput + " | Your Input " + userInput);
+				} else {
+					loss++;
+					System.out.println("LOSS" + " | Computer Input: " + cpuInput + " | Your Input " + userInput);
+				}
+			} else {
+				if (userInput >= 2) {
+					if (cpuInput == 1 && userInput == 3) {
+						loss++;
+						System.out.println("loss" + " | Computer Input: " + cpuInput + " | Your Input " + userInput);
+					} else {
+						win++;
+						System.out.println("WIN" + " | Computer Input: " + cpuInput + " | Your Input " + userInput);
+					}
+				}
+			}
+			if (userInput == 0) {
+				System.out.println("wins " + win);
+				System.out.println("loss " + loss);
+				System.out.println("tie " + tie);
+				con = false;
+			}
+		}
+
+	}
+
+}
