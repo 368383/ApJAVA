3c059430de46137caa9a0450aeb5e6527f782a14
368383 <368383@427-NA307-032.mcpsmd.org>
2019-09-17 08:22:26 -0400

    FULL POINTS

--- 368383/ApJAVA.git/APJava/src/quarter1/factorial.java
+++ 368383/ApJAVA.git/APJava/src/quarter1/factorial.java
@@ -6,13 +6,22 @@ public class factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		int fi = 1;
-		for(int i=1; i<=input;i++) {
-			fi=fi*i;
+		boolean i = true;
+		while (i) {
+			Scanner sc = new Scanner(System.in);
+			int input = sc.nextInt();
+			if (input < 0) {
+				System.out.println("INPUT POS INT");
+				continue;
+			} else {
+				int fi = 1;
+				for (int i1 = 1; i1 <= input; i1++) {
+					fi = fi * i1;
+				}
+				System.out.println("int " + input + " factorial is " + fi);
+				i = false;
+			}
 		}
-		System.out.println("int "+ input + " factorial is " + fi);
 	}
 
 }
