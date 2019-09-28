f60baa59737c37eb542084176573e0da961ca532
Jonathan Young <Jonathan Young@S340>
2019-09-28 00:44:31 -0400

    Final clean version

--- 368383/ApJAVA.git/APJava/src/quarter1/pigLatinCleanedVersion.java
+++ 368383/ApJAVA.git/APJava/src/quarter1/pigLatinCleanedVersion.java
@@ -2,19 +2,29 @@ package quarter1;
 
 import java.util.Scanner;
 
-public class pigLatin {
+public class pigLatinCleanedVersion {
+
+	private static boolean stopProgram = false;
+	private static String invalid = "INVALD";
+	private static String startsWithVowel = "startsWithVowel";
+//private static String doubleY = "doubleY";
+	private static String containsQU = "containsQU";
+	private static String defaultCase = "defaultCase";
 
 	public static void main(String[] args) {
-		while (1 == 1) {
+		while (true) {
 			System.out.println("PLEASE TYPE IN YOUR TEXT. TYPE IN 'STOP' TO STOP PROGRAM ");
 			String original = prompt();
-			String owl = firstPunct(original);
-			String fox = secondPunct(outputCaps(capital(owl), process(owl)), original);
-			if (fox.equals("stop")) {
+			String[] inputCleaned = removePunctionAtEnd(original);
+			boolean startsWithCap = isFirstCapital(inputCleaned[0]);
+			String reArrangedInput = reArrange(inputCleaned[0]);
+			String capInput = restoreCaps(startsWithCap, reArrangedInput);
+			String finalOutPut = capInput + inputCleaned[1];
+			if (stopProgram) {
 				System.out.println("PROGRAM HAS FINISHED ");
 				break;
 			} else {
-				System.out.println(fox);
+				System.out.println(finalOutPut);
 			}
 		}
 	}
@@ -23,13 +33,14 @@ public class pigLatin {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.next();
 		if (input.toLowerCase().contains("stop")) {
+			stopProgram = true;
 			return "stop";
 		} else {
 			return input;
 		}
 	}
 
-	public static boolean capital(String input) {
+	public static boolean isFirstCapital(String input) {
 		String wolves = input;
 		if (wolves.substring(0, 1).equals(wolves.substring(0, 1).toUpperCase())) {
 			return true;
@@ -37,29 +48,19 @@ public class pigLatin {
 		return false;
 	}
 
-	public static String secondPunct(String input, String finalized) {
-		return input + type(finalized);
-	}
+	public static String[] removePunctionAtEnd(String input) {
+		String returnValue[] = { input, "" };
 
-	public static String firstPunct(String input) {
-		String wolves = input;
-		String check = wolves.substring(wolves.length() - 1, wolves.length());
-		if (check.equals("!") || check.equals("?") || check.equals(".")) {
-			return wolves.substring(0, wolves.length() - 1);
+		String lastChar = input.substring(input.length() - 1, input.length());
+		if (lastChar.equals("!") || lastChar.equals("?") || lastChar.equals(".")) {
+			returnValue[0] = input.substring(0, input.length() - 1);
+			returnValue[1] = input.substring(input.length() - 1);
 		}
-		return wolves;
+
+		return returnValue;
 	}
 
-	public static String type(String input) {
-		String wolves = input;
-		String check = wolves.substring(wolves.length() - 1, wolves.length());
-		if (check.equals("!") || check.equals("?") || check.equals(".")) {
-			return wolves.substring(wolves.length() - 1);
-		}
-		return "";
-	}
-
-	public static String outputCaps(boolean caps2, String punctuation) {
+	public static String restoreCaps(boolean caps2, String punctuation) {
 		String birds = punctuation;
 		if (caps2 == true) {
 			return birds.substring(0, 1).toUpperCase() + birds.substring(1).toLowerCase();
@@ -67,75 +68,90 @@ public class pigLatin {
 		return birds;
 	}
 
-	public static String process(String input) {
+	public static String reArrange(String input) {
+
 		if (input.contains("stop")) {
 			return "stop";
 		} else {
-			int cases = cases(input);
+			String cases = cases(input);
 			switch (cases) {
-			case 0:
+			case "startsWithVowel":
 				return input + "way";
-			case 1:
-				String addition = input + "ii";
-				for (int i = 0; i < addition.length() - 2; i++) {
-					String seq = addition.substring(i, i + 1);
-					if ((seq.equals("a") || seq.equals("e") || seq.equals("i") || seq.equals("o") || seq.equals("u"))) {
+
+			case "containsQU":
+				int index = input.indexOf("qu");
+				if (index != -1) {
+					return input.substring(index + 2) + input.substring(0, index) + "qu" + "ay";
+				} else {
+					System.out.println("Invalid 2" + input);
+					return invalid;
+
+				}
+				/*
+				 * case "doubleY": for (int i = 0; i < input.length() - 1; i++) { String seq =
+				 * input.substring(i, i + 1); if (input.substring(0, 1).equals("y")) { if
+				 * (containsVowel(seq)) { return input.substring(1) + "yay"; }
+				 * 
+				 * } else { for (int i1 = 0; i1 < input.length() - 1; i1++) { if
+				 * (input.charAt(i1) == 'y') { return input.substring(i1) + input.substring(0,
+				 * i1) + "ay"; } } System.out.println("Invalid 1" + input); return invalid; } }
+				 */
+
+			case "invalid":
+				System.out.println("Invalid 3" + input);
+
+				return invalid;
+			default:
+				for (int i = 0; i < input.length(); i++) {
+					String seq = input.substring(i, i + 1);
+					if (containsYVowel(seq, i)) {
 						return input.substring(i) + input.substring(0, i) + "ay";
 					}
 				}
-			case 2:
-				int index = input.indexOf("qu");
-				return input.substring(index + 2) + input.substring(0, index) + "qu" + "ay";
-			case 3:
-				for (int i = 0; i < input.length(); i++) {
-					String seq = input.substring(i, i + 1);
-					if (input.substring(0, 1).equals("y") && (seq.equals("a") || seq.equals("e") || seq.equals("i")
-							|| seq.equals("o") || seq.equals("u"))) {
-						return input.substring(1) + "yay";
-					} else {
-						for (int k = 0; k < input.length(); k++) {
-							if (input.substring(k, k + 1).contentEquals("y")) {
-								return input.substring(k) + input.substring(0, k) + "ay";
-							}
-						}
-					}
-				}
-			case 10:
-				return "INVALD";
+				return invalid;
 			}
 		}
-		return input;
 	}
 
-	public static int cases(String eagle) {
-		String input = eagle.toLowerCase();
-		boolean valid = false;
-		for (int i = 0; i < input.length(); i++) {
+	private static boolean containsVowel(String input) {
+		return input.equals("a") || input.equals("e") || input.equals("i") || input.equals("o") || input.equals("u");
+	}
+
+	private static boolean containsYVowel(String input, int currentIndex) {
+		if (input.equalsIgnoreCase("y") && currentIndex == 0) {
+			return false;
+		} else {
+			return (input.equalsIgnoreCase("y") || containsVowel(input));
+		}
+
+	}
+
+	public static String cases(String input) {
+		input = input.toLowerCase();
+		boolean hasVowels = false;
+		for (int i = 0; i < input.length() - 1; i++) {
 			String seq = input.substring(i, i + 1);
-			if (input.substring(i, i + 1).contains("y") && !(input.substring(0, 1).contains("y"))) {
-				return 3;
-			}
-			if (seq.equals("a") || seq.equals("e") || seq.equals("i") || seq.equals("o") || seq.equals("u")
-					|| seq.equals("y") || seq.equals("i")) {
-				valid = true;
-				break;
+			/*
+			 * if (seq.contains("y") && !(input.substring(0, 1).contains("y"))) { return
+			 * doubleY; }
+			 */
+			if (containsVowel(seq) || seq.equals("y")) {
+				hasVowels = true;
 			} else {
-				if (valid == false && i == input.length() - 1) {
-					return 10;
+				if (hasVowels == false && i == input.length() - 1) {
+					return invalid;
 				}
 			}
 		}
-		if (input.substring(0, 1).equals("a") || input.substring(0, 1).equals("e") || input.substring(0, 1).equals("i")
-				|| input.substring(0, 1).equals("o") || input.substring(0, 1).equals("u")) {
-			return 0;
+		if (containsVowel(input.substring(0, 1))) {
+			return startsWithVowel;
 		}
 
-		String addition = input + "ii";
 		for (int i = 0; i < input.length() - 2; i++) {
-			if (addition.substring(i, i + 2).contains("qu")) {
-				return 2;
+			if (input.substring(i, i + 2).contains("qu")) {
+				return containsQU;
 			}
 		}
-		return 1;
+		return defaultCase;
 	}
 }
