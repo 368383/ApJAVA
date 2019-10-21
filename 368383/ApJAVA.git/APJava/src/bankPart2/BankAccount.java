7f6f95762a8c8cf098e4647c33de9830c76cdca4
368383 <368383@427-NA307-032.mcpsmd.org>
2019-10-21 07:49:56 -0400

    minor

--- 368383/ApJAVA.git/APJava/src/bankPart2/BankAccount.java
+++ 368383/ApJAVA.git/APJava/src/bankPart2/BankAccount.java
@@ -33,12 +33,12 @@ public class BankAccount {
 		return "Customer Last Name " + "<" + lastName + ">" + " | Customer Balance " + "<" + balance + ">";
 	}
 
-	public String toFile() {
-		return lastName + "   " + balance + "\n";
-	}
-
 	public double getBalance() {
 		return balance;
 	}
 
+	// optional
+	public String toFile() {
+		return lastName + "   " + balance + "\n";
+	}
 }
