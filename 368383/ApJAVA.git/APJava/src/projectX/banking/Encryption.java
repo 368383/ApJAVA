18dd29b20c9fb60356e19d8c5cc95eb3cfe6a67d
jonat <jonat@SIGMA-VOYAGER>
2019-10-22 21:40:33 -0400

    Encryption technique works, implementation is wack.

--- 368383/ApJAVA.git/APJava/src/projectX/banking/Encryption.java
+++ 368383/ApJAVA.git/APJava/src/projectX/banking/Encryption.java
@@ -10,22 +10,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Encryption {
+
+	private static int forKey = 0;
 	private static ArrayList<BankAccount> encryptionHolder = new ArrayList<BankAccount>();
 
 	public static void encrypt() {
-		// for (BankAccount currentAccount : encryptionHolder) {
-		for (int i = 0; i < 1; i++) {
-			// String convert = currentAccount.toFile();
-			byte[] ascii = "BOB THE BUILDER".getBytes(StandardCharsets.US_ASCII);
-			String asciiString = Arrays.toString(ascii);
-			System.out.println(asciiString);
-			long encryptedArray = Integer.valueOf(asciiString) * keyGenerator();
 
-		}
+		// String convert = currentAccount.toFile();
+		forKey = keyGenerator();
+		System.out.println(forKey);
+		byte[] ascii = "A".getBytes(StandardCharsets.US_ASCII);
+		String asciiString = Arrays.toString(ascii);
+		System.out.println(asciiString);
+
+		float encryptedArray = Integer.valueOf(asciiString.substring(1, asciiString.length()-1)) * forKey;
+		System.out.println(encryptedArray);
 	}
 
 	public static int keyGenerator() {
-		int value = (int) Math.random() * 100;
+		int value = (int) (Math.random() * 100) + 1;
 		return value;
 	}
 }
