0d022afc295ace85b96c5d6b797c8ced0cc2e378
semperexplo <368383@users.noreply.github.com>
2019-11-20 20:20:52 -0500

    UP TO DATE WITH BRANCH SCHOOL COMPUTER

---
+++ 368383/ApJAVA.git/APJava/src/projectX/WrapperTestLaunch.java
@@ -0,0 +1,12 @@
+package projectX;
+
+public class WrapperTestLaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WrapperTest alpha = new WrapperTest();
+		alpha.beta=40;
+		System.out.println(alpha.beta);
+	}
+
+}
