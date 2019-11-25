07e76c5b00cf6f021a9dcb5933d5cac4db683d04
368383 <368383@427-NC130-079.mcpsmd.org>
2019-11-25 09:12:24 -0500

    solved, but not to true functional capacity.

--- 368383/ApJAVA.git/APJava/src/projectX/MathLeagueqQ10.java
+++ 368383/ApJAVA.git/APJava/src/projectX/MathLeagueqQ10.java
@@ -2,11 +2,9 @@ package projectX;
 
 public class MathLeagueqQ10 {
 
-	public static double increment = 0.01;
-	public static double x = 0.08;
-	public static double y = 0;
-	public static double baseValue = -5;
-	public static double limitValue = 5;
+	public static double increment = 0.0000000000001;
+	public static double baseValue = 0;
+	public static double limitValue = 0.25;
 	public static boolean solved = false;
 	public static double smallest = 0;
 
@@ -49,56 +47,64 @@ public class MathLeagueqQ10 {
 	private static double recursive2(double lowerValue, double upperValue) {
 		baseValue = lowerValue;
 
-		System.out.println("NEXT SEQUENCE " + "=============================================");
-		System.out.println("Upper Value " + upperValue + "\tBase Value " + baseValue + "\t Lower Value" + lowerValue);
-		System.out.println("_____________________________________________");
+		// System.out.println(
+		// "=======================" + "NEXT SEQUENCE " +
+		// "=============================================");
+		// System.out.println("Upper Value " + upperValue + "\tBase Value " + baseValue
+		// + "\t Lower Value" + lowerValue);
+		// System.out.println("_____________________________________________");
 
 		if (upperValue - lowerValue < increment) {
-			System.out.println("less than condition "
-					+ Math.abs(Math.abs(sum(lowerValue, y(lowerValue))) - Math.abs(sum(upperValue, y(upperValue)))));
-			System.out.println("PRECISION LIMIT");
-			return 0;
+			// System.out.println("less than condition "
+			// + Math.abs(Math.abs(sum(lowerValue, y(lowerValue))) -
+			// Math.abs(sum(upperValue, y(upperValue)))));
+			// System.out.println("PRECISION LIMIT");
+			solved = true;
+			System.out.println("LOWER VALUE " + lowerValue);
+			return lowerValue;
 		}
 
 		double domain = upperValue - lowerValue;
-		System.out.println("domain " + domain);
+		// System.out.println("domain " + domain);
 		for (int i = 0; i < 10; i = i + 1) {
 			double base = baseValue + (domain / 10) * i;
 			double top = baseValue + (domain / 10) * (i + 1);
 			double LowerTolerance = sum(base, y(base));
 			double UpperTolerance = sum(top, y(top));
-			System.out.println();
-			System.out.println("ORDERED PAIRS\t" + "(" + base + "," + LowerTolerance + ")" + "\t" + "(" + top + ","
-					+ UpperTolerance + ")");
-			if (UpperTolerance - LowerTolerance < 0) {
-				System.out.println("CHANGE DETECTED | " + "\t LOWER: " + base + "\t UPPER: " + top);
+
+			// System.out.println("ORDERED PAIRS\t" + "(" + base + "," + LowerTolerance +
+			// ")" + "\t" + "(" + top + ","
+			// + UpperTolerance + ")");
+			if (UpperTolerance - LowerTolerance > 0) {
+//				System.out.println("CHANGE DETECTED  x | " + "\t LOWER: " + base + "\t UPPER: " + top);
+//				System.out.println(
+//						"CHANGE DETECTED  y | " + "\t LOWER X: " + LowerTolerance + "\t UPPER: " + UpperTolerance);
+
 				recursive2(base, top);
 				break;
 			} else {
 			}
 		}
-		System.out.println("PROGRAM ERROR");
 		return 0;
-
 	}
 
 	private static void loopMethod() {
-
-		double sum = 0;
-		double i = increment;
-		y(i);
-		double smallest = sum(i, y);
-
-		for (i = 0.001; i < 0.25; i = increment + i) {
-			y(i);
-			sum = sum(i, y);
-			if (sum < smallest) {
-				smallest = sum;
-			} else {
-				break;
-			}
-		}
-		System.out.println("X value " + i + "\tSUM: " + sum);
+//
+//		double sum = 0;
+//		double i = increment;
+//		y(i);
+//		double smallest = sum(i, y);
+//
+//		for (i = 0.001; i < 0.25; i = increment + i) {
+//			y(i);
+//			sum = sum(i, y);
+//			if (sum < smallest) {
+//				smallest = sum;
+//			} else {
+//				break;
+//			}
+//		}
+//		System.out.println("X value " + i + "\tSUM: " + sum);
 	}
 
 	public static double y(double x) {
